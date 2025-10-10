be9bc34b24a6daf45b3fa74e2b94e5f575c652dd
AkaiiTenshi <soysal.salih93@gmail.com>
2025-10-10 15:10:32 +0200

    used toupper

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,3 +1,16 @@
-int main(int ac, char **av){
+#include <cctype>
+#include <iostream>
 
+using namespace std;
+
+int main(int ac, char **av){
+    char c = av[1][1];
+    if (ac > 1)
+    {
+        std::cout << c << std::endl;
+       c = toupper(c);
+        std::cout << c << std::endl;
+    }
+    std::cout << "Test" << std::endl;
+    return 0;
 }
